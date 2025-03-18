289324d03cd8b760c1d2b406e337f034f9448f4a
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-03-19 03:18:48 +0330

    add ProgressBar & NextButton

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,15 +6,17 @@ import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
-
   // loading, ready, error , active , finished
   status: "loading",
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -41,17 +43,23 @@ function reducer(state, action) {
         index: state.index + 1,
         answer: null,
       };
+    case "finished":
+      return { ...state, status: "finished" , highscore: state.points > state.highscore ? state.points : state.highscore };
 
     default:
       throw new Error("Action unknown");
   }
 }
 function App() {
-  const [{ questions, status, index, answer }, dispatch] = useReducer(
+  const [{ questions, status, index, answer, points , highscore }, dispatch] = useReducer(
     reducer,
     initialState
   );
   const numQuestions = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, curr) => prev + curr.points,
+    0
+  );
 
   useEffect(() => {
     fetch("http://localhost:8000/questions")
@@ -71,13 +79,29 @@ function App() {
           )}
           {status === "active" && (
             <>
+              <Progress
+                index={index}
+                numQuestions={numQuestions}
+                points={points}
+                maxPossiblePoints={maxPossiblePoints}
+                answer={answer}
+              />
               <Question
                 question={questions[index]}
                 dispatch={dispatch}
                 answer={answer}
                 index={index}
               />
-              <NextButton dispatch={dispatch} answer={answer} />
+              <NextButton dispatch={dispatch} answer={answer} index={index} numQuestions={numQuestions} />
+            </>
+          )}
+          {status === "finished" && (
+            <>
+              <FinishScreen
+                points={points}
+                maxPossiblePoints={maxPossiblePoints} 
+                highscore={highscore}
+              />
             </>
           )}
         </Main>
---
+++ src/components/Progress.jsx
@@ -0,0 +1,13 @@
+const progress = ({index, numQuestions,answer , points , maxPossiblePoints }) => {
+  return (
+    <header className="progress">
+        <progress max={numQuestions} value={index + Number(answer !== null)}></progress>
+      <p>
+        Question <strong>{index+1}</strong>  / {numQuestions}
+      </p>
+      <p><strong>{points}</strong> / {maxPossiblePoints}</p>
+    </header>
+  );
+};
+
+export default progress;
