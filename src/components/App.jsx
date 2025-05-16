20b23f27359aa0dd44edb06d76f27e5804f6f1d4
Mohammad Javad Rezaei <mjr0924@gmail.com>
2025-05-16 21:24:02 +0330

    change data path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -81,7 +81,7 @@ function App() {
   );
 
   useEffect(() => {
-    fetch("/data/questions.json")
+    fetch("/questions.json")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataReceived", payload: data.questions }))
       .catch((err) => dispatch({ type: "dataFailed" }));
